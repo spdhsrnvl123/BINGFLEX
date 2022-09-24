cabb67e1f7e4f7d860a796a2602e5c82fdf11131
이태형 <spdhsrnvl123@naver.com>
2022-09-24 19:08:40 +0900

    🎉메인화면 UI 작업

--- src/components/base/HomeFirst.jsx
+++ src/components/base/HomeFirst.jsx
@@ -1,139 +1,65 @@
-import { useState, useEffect } from "react";
-import { Rate, Button } from "antd";
-import { PlayCircleOutlined } from "@ant-design/icons";
 import styled from "styled-components";
+import { CaretRightOutlined,PlusOutlined } from '@ant-design/icons';
 
 const Main = styled.div`
-  .header_content {
-    height: 75vh;
-    width: 100%;
-    background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)),
-      url(/assets/background.jpg);
+    height: 100vh;
+    background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.2)),url(/assets/background.jpeg);
     background-repeat: no-repeat;
-    background-size: 100%;
-    background-position: center;
-  }
+    background-size: cover;
+`
 
-  .header_content .loading {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -120%);
-    width: 80px;
-    height: 80px;
-  }
+const TilteContainer = styled.div`
+  position:relative;
+  top:25%;
+  left:10%;
+  width:50%;
+  height: 50%;
+  color: white;
+`
 
-  .header_container {
-    color: white;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-74%, -60%);
-    display: grid;
-    grid-template-columns: repeat(2, minmax(30px, auto));
-  }
-  .header_container .main_image {
-    height: 410px;
-    width: 250px;
-    border-top-left-radius: 30px;
-    border-bottom-left-radius: 30px;
-    box-shadow: 0px 30px 60px -12px rgba(50, 50, 93, 0.25),
-      0 18px 36px -18px rgba(0, 0, 0, 0.3),
-      0 -12px 36px -8px rgba(0, 0, 0, 0.025);
-    position: relative;
-    z-index: 100;
-    cursor: pointer;
-  }
-  .header_container .content {
-    display: grid;
-    grid-template-rows: repeat(4, minmax(20px, auto));
-    background: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.4));
-    padding: 20px;
-  }
-  .header_container .content .genres_list_container {
-    display: grid;
-    grid-template-columns: repeat(3, minmax(100px, auto));
-  }
-  .header_container .content .genres_list_container .genres_list:before {
-    content: "⌘";
-    margin-right: 0.5rem;
-  }
-  .header_content_footer {
-    background-image: linear-gradient(
-      to right bottom,
-      #051937,
-      #171228,
-      #190a1a,
-      #12040d,
-      #000000
-    );
-    height: 25vh;
-  }
+const Title = styled.h1`
+  position : absolute;
+  font-size : 110px;
+  text-shadow: 2px -3px 3px black;
+`;
+const TitleDesCription = styled.p`
+  position: absolute;
+  top: 30%;
+  left:1.5%;
+  color:#dcdde1;
 `;
 
-const HomeFirst = () => {
-  const [movie, setMovie] = useState([]);
-  const [start, startSetting] = useState(true);
+const PlayButton = styled.button`
+  position:absolute;
+  display: flex;
+  top: 53%;
+  left:1.5%;
+  color:white;
+  background-color: #f53b57;
+  padding:12px;
+  border:0;
+  font-size: 16px;
+  border-radius: 20px;
+  font-weight: bold;
+`
 
-  const update = async () => {
-    const item = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-      )
-    ).json();
-    setMovie(item.data.movies);
-    startSetting(false);
-  };
-  useEffect(() => {
-    update();
-  }, []);
+const ListButton = styled(PlayButton)`
+  left:23.5%;
+`
 
-  const summary =
-    "Jai Bhim (transl. Victory to Bhim) is a 2021 Indian Tamil-language legal drama film directed by T. J. Gnanavel and produced by Jyothika and Suriya under 2D Entertainment. The film stars Suriya and Lijomol Jose and Manikandan with Rajisha Vijayan, Prakash Raj, Rao Ramesh and others in supporting roles. The film deals with the subject of police bias and state violence against a marginalised community.";
-  //   console.log(movie);
-  return (
+const HomeFirst = ()=>{
+  return(
     <Main>
-      <div className="header_content">
-        {start ? (
-          <img className="loading" src="/assets/loading2.gif" alt="loading" />
-        ) : (
-          <div className="header_container">
-            <img
-              className="main_image"
-              src={movie[0].medium_cover_image}
-              alt="movie_images"
-            />
-            <div className="content">
-              <div style={{ fontSize: "1.8rem" }}>{movie[0].year}</div>
-              <div style={{ fontSize: "2.3rem" }}>{movie[0].title}</div>
-              <div style={{ fontSize: "1.2rem", lineHeight: "25px" }}>
-                {summary.slice(0, 303)}
-              </div>
-              <ul className="genres_list_container">
-                {movie[0].genres.map((value, index) => {
-                  return (
-                    <li className="genres_list" key={index}>
-                      {value}
-                    </li>
-                  );
-                })}
-              </ul>
-              <Rate
-                style={{ paddingBottom: "10px" }}
-                allowHalf
-                defaultValue={3.5}
-              />
-              <Button size="large" type="primary" danger>
-                <PlayCircleOutlined />
-                Watch trailer
-              </Button>
-            </div>
-          </div>
-        )}
-      </div>
-      <div className="header_content_footer"></div>
+      <TilteContainer>
+        <Title>INTERSTELLAR</Title>
+        <TitleDesCription>
+          Based on the theory of theoretical physicist Kip Thorne, the film tells the story of a group<br />
+          of explorers through the wormhole about time travel and parallel universe...
+        </TitleDesCription>
+        <PlayButton><CaretRightOutlined />WATCH NOW</PlayButton>
+        <ListButton><PlusOutlined />MY LIST</ListButton>
+      </TilteContainer>
     </Main>
-  );
-};
-
+  )
+}
 export default HomeFirst;
