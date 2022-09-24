af1f82bed96f42a472541b3df3cae0e0c19641e3
이태형 <spdhsrnvl123@naver.com>
2022-09-24 19:09:58 +0900

    🎉메뉴바 UI 수정 작업

--- src/components/base/Menu.jsx
+++ src/components/base/Menu.jsx
@@ -15,28 +15,33 @@ const Container = styled.div`
     z-index: 9999;
     display:  flex;
     justify-content: space-between;
-    background-color: black;
+    background-color: rgba(0,0,0,0.4);
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
     box-shadow: 2px 2px 5px hsla(0, 0%, 0%, 0.443);
 `
+
 const Content = styled.ul`
     display: flex;
     justify-content: center;
     align-items: center;
+    /* background-color: red;   */
     li{
         font-size: 20px;
         padding: 20px;
         &:hover{
-            text-decoration: underline;
+            transform: scale(1.2);
+            transition:0.3s;
         }
     }
 `
+
 const Title = styled.div`
     font-size: 35px;
     display: flex;
     align-items: center;
 `
+
 const Logo = styled.div`
     background-image: url("./assets/logo.png");
     background-size: cover;
@@ -50,20 +55,20 @@ const Menu = ()=>{
     
     return(
         <TopCotainer>
-        <Container>
-            <Title><Logo /><Link>INGFLEX</Link></Title>
-            <Content>
-                <li><Link>MAIN</Link></li>
-                <li><Link>SCHEDLUES</Link></li>
-                <li><Link>TICKETS</Link></li>
-                <li><Link>NEWS</Link></li>
-                <li><Link>CONTACT</Link></li>
-            </Content>
-            <Content>
-                <Link to={`/login`} style={{fontSize:"20px",marginRight:"10px"}}>Login</Link>
-                <Link to={`/signup`} style={{fontSize:"20px"}} >Sign Up</Link>
-            </Content>
-        </Container>
+            <Container>
+                <Title><Logo /><Link>INGFLEX</Link></Title>
+                <Content>
+                    <li><Link>MAIN</Link></li>
+                    <li><Link>SCHEDLUES</Link></li>
+                    <li><Link>TICKETS</Link></li>
+                    <li><Link>NEWS</Link></li>
+                    <li><Link>CONTACT</Link></li>
+                </Content>
+                <Content>
+                    <Link to={`/login`} style={{fontSize:"20px",marginRight:"10px"}}>Login</Link>
+                    <Link to={`/signup`} style={{fontSize:"20px", marginRight:"14px"}}>Sign Up</Link>
+                </Content>
+            </Container>
         </TopCotainer>
     )
 }
