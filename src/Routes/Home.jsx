f8aab2a8e165aa0da450eea876ee1652f1daca6b
이태형 <spdhsrnvl123@naver.com>
2022-10-11 23:53:53 +0900

    🎉두번째 섹션 Framer motion을 이용한 애니메이션 틀 구현

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,21 +1,127 @@
 import styled from "styled-components";
-import HomeFirst from "../components/HomeFirst"
+import FirstScene from "../components/FirstScene"
+import ScollDown from "../components/ScrollDown"
+// import Main from "../components/Main"
+import getData from "../api"
+import { useQuery } from "@tanstack/react-query"
+import { motion, useMotionValue,useTransform } from "framer-motion";
+import Main from "../components/Main";
+
+const FirstSection = styled.div`
+    height: 120vh;
+    width:100%;
+    background-color: black;
+    overflow-x: hidden;
+`
+
+const SecondSection = styled(FirstSection)`
+    position: relative;
+`
 
 const Section = styled.div`
     height: 100vh;
-    width: 100%;
-    background-color: #4B4453;
+    /* width: 100%; */
+    background-color:black;
+    overflow-x: hidden;
+    border-top: 10px dotted white;
+`
+const Box = styled(motion.div)`
+    width:300px;
+    height: 300px;
+    background-color: red;
+    position: relative;
+    top:50%;
+    left:50%;
+    border-radius: 50%;
+    /* border-radius: 50%; */
+`
+
+const Circle = styled(motion.div)`
+    width:200px;
+    height:200px;
+    border-radius: 50%;
+    background-color: blue;
+    font-size : 50px;
+    position: absolute;
+    top:-50%;
+    left:50%;
+    transform: translate(-50%,-50%);
+`
+const Circle2 = styled(Circle)`
+    background-color: white;
+    left:-50%;
+    top:50%;
+`
+const Circle3 = styled(Circle)`
+    background-color: yellow;
+    top:50%;
+    left:150%;
+`
+
+const Circle4 = styled(Circle)`
+    background-color: skyblue;
+    top:150%;
+    left:50%;
 `
 
 const Home = ()=>{
+    const { data, isLoading } = useQuery(["movie","nowPlaying"],getData)
+    // const y = useMotionValue(0);
+    // useEffect(()=> x.onChange(latest => console.log(x.get()),[]))
+    console.log(data,isLoading)
+
+    const rotateVariants = {
+        start : {
+            rotate : 0,
+            x : -150,
+            y : -150
+        },
+        end : {
+            rotate : 360,
+            transition : {
+                duration:10,
+                repeat : Infinity             
+            }
+        },
+    }
+
+    const moveVariants = {
+        
+        click : {
+            left:150,
+            width: 280,
+            height :280
+        },
+        click2 :{
+            top:150,
+            width: 280,
+            height :280
+        },
+    }
+
     return(
         <>
+            <FirstSection>
+                <FirstScene />
+                <ScollDown />
+            </FirstSection>
+            <SecondSection>
+                <Box
+                    variants={rotateVariants}
+                    initial="start"
+                    animate="end"
+                >
+                    <Circle variants={moveVariants} whileTap="click2" />
+                    <Circle2 variants={moveVariants} whileTap="click" />
+                    <Circle3 variants={moveVariants} whileTap="click" />
+                    <Circle4 variants={moveVariants} whileTap="click2" />
+                </Box>
+            </SecondSection>
             <Section>
-                <HomeFirst />
+                <Main />
             </Section>
-            <Section />
-            <Section />
-            <Section />
+            <Section>
+            </Section>
         </>
     )
 }
